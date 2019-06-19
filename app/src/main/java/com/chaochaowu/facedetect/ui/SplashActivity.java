fed7898c964d5b257029869bdcd7dacc273ccc78
Metero、 <704734514@qq.com>
2019-06-19 22:00:47 +0800

    所有功能 完成

--- app/src/main/java/com/chaochaowu/facedetect/ui/SplashActivity.java
+++ app/src/main/java/com/chaochaowu/facedetect/ui/SplashActivity.java
@@ -21,12 +21,12 @@ public class SplashActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_splash);
 		getSupportActionBar().hide();
 
-		splash_text = (TextView)findViewById(R.id.splash_text);
-		splash_text.setText(
-						"喜欢一个人,\n" +
-						"始于颜值,\n" +
-						"陷于才华,\n" +
-						"忠于人品,\n");
+//		splash_text = (TextView)findViewById(R.id.splash_text);
+//		splash_text.setText(
+//						"喜欢一个人,\n" +
+//						"始于颜值,\n" +
+//						"陷于才华,\n" +
+//						"忠于人品,\n");
 		handler = new Handler();
 		// 延迟SPLASH_DISPLAY_LENGHT时间然后跳转到MainActivity
 		handler.postDelayed(new Runnable() {
